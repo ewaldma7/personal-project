9b2c634de135008c21dbb2f29ae922fff3d15096
ewaldma7 <ewaldma7@jhu.edu>
2024-05-21 20:03:06 -0400

    add friend request notification feature

--- app/api/friends/route.ts
+++ app/api/friends/route.ts
@@ -1,6 +1,13 @@
 import prisma from "@/app/lib/client";
 import { requestFriendSchema, respondFriendSchema } from "@/app/validationSchemas";
+import { Status } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+
+type FriendWhereFilterType = {
+    user_id: number;
+    status: Status;
+    user_requested?: boolean;
+};
 
 export async function GET(request: NextRequest) {
     try {
@@ -14,15 +21,25 @@ export async function GET(request: NextRequest) {
               });          
             return NextResponse.json(allFriends, { status: 200 });
         }
-        const user_id = params.get('user_id')
-        if (!user_id)  return NextResponse.json('No User ID provided!', { status: 400 });
+        const user_id = params.get('user_id');
+        const status = params.get('status');
+        if (!user_id || !status)  return NextResponse.json('No User ID provided!', { status: 400 });
+        const whereFilter : FriendWhereFilterType = {user_id: parseInt(user_id),
+            status: status as Status}
+        if (status === 'PENDING') {
+            whereFilter.user_requested = false;
+        }
         const friends = await prisma.friend.findMany({
-            where: {
-                user_id: parseInt(user_id),
-                status: "ACCEPTED"
-            },
+            where: whereFilter,
             include: {
-                friend: true
+                friend: {
+                    select: {
+                        email: true,
+                        location: true,
+                        name: true,
+                        user_id: true
+                    }
+                }
             }
         });
         return NextResponse.json(friends, {status: 200});
@@ -36,8 +53,9 @@ export async function POST(request: NextRequest) {
     try {
         const body = await request.json();
         const validation = requestFriendSchema.safeParse(body);
+        console.log(validation.success)
         if (!validation.success) {
-            return NextResponse.json(validation.error.format(), { status: 400 });
+            return NextResponse.json('Invalid email address', { status: 400 });
         }
         const requestedFriend = await prisma.user.findUnique({
             where: {
@@ -59,8 +77,8 @@ export async function POST(request: NextRequest) {
             const message = existingFriend.status === 'ACCEPTED' ? 'Cannot send request to existing friend' : 'Cannot send request multiple times';
             return NextResponse.json(message, { status: 400 });
         }
-        const newRequest = await prisma.friend.create({ data: { user_id: body.user_id, friend_id: requestedFriend.user_id }, include: {friend: true} });
-        const newRequest2 = await prisma.friend.create({ data: { user_id: requestedFriend.user_id, friend_id: body.user_id } });
+        const newRequest = await prisma.friend.create({ data: { user_id: body.user_id, friend_id: requestedFriend.user_id, user_requested: true }, include: {friend: true} });
+        const newRequest2 = await prisma.friend.create({ data: { user_id: requestedFriend.user_id, friend_id: body.user_id, user_requested: false  } });
         return NextResponse.json(newRequest, { status: 201 });
     } catch (error) {
         console.log(error);
@@ -71,6 +89,7 @@ export async function POST(request: NextRequest) {
 export async function PUT(request: NextRequest) {
     try {
         const body = await request.json();
+        console.log(body)
         const validation = respondFriendSchema.safeParse(body);
         if (!validation.success) {
             return NextResponse.json(validation.error.format(), { status: 400 });
@@ -86,11 +105,11 @@ export async function PUT(request: NextRequest) {
         if (!existingRequest) {
             return NextResponse.json('There is no pending request', { status: 400 });
         }
-        const newRequest = await prisma.friend.update({ data: {status: body.status}, where: { user_id_friend_id: {
+        const newRequest = await prisma.friend.update({ data: {status: body.accepted ? 'ACCEPTED' : 'DECLINED'}, where: { user_id_friend_id: {
             user_id: body.user_id,
             friend_id: body.friend_id
           } } });
-        const newRequest2 = await prisma.friend.update({ data: {status: body.status}, where: { user_id_friend_id: {
+        const newRequest2 = await prisma.friend.update({ data: {status: body.accepted ? 'ACCEPTED' : 'DECLINED'}, where: { user_id_friend_id: {
         user_id: body.friend_id,
         friend_id: body.user_id
         } } });
