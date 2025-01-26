27a768f19515876997d30825ba2614fb56eb4dd0
ewaldma7 <ewaldma7@jhu.edu>
2025-01-26 14:59:25 -0500

    add loading icon for register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,165 +1,221 @@
-'use client'
+"use client";
 
-import { useState } from "react"
+import { useState } from "react";
 import axios from "axios";
 import Link from "next/link";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 export default function Register() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirm_password, setConfirmPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
-    const validPassword = (password && confirm_password && (password === confirm_password)) ? true : false;
-    const router = useRouter();
-    
-    const logIn = async () => {
-      const res = await signIn("credentials", {
-        username: email,
-        password: password,
-        redirect: false,
-        callbackUrl: "/dashboard"
-      });
-      if (res?.ok) {
-        // Login successful
-        router.push("/dashboard");
-      } else {
-        // Login failed
-        console.error("Login failed");
-      }
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm_password, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const validPassword =
+    password && confirm_password && password === confirm_password;
+  const router = useRouter();
+
+  const logIn = async () => {
+    const res = await signIn("credentials", {
+      username: email,
+      password: password,
+      redirect: false,
+      callbackUrl: "/dashboard",
+    });
+    if (res?.ok) {
+      // Login successful
+      router.push("/dashboard");
+    } else {
+      // Login failed
+      console.error("Login failed");
     }
+  };
 
-    const registerUser = async (e: React.FormEvent) => {
-        e.preventDefault();
-        try {
-          const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
-            name: name,
-            email: email,
-            password: password
-          });
-          await logIn();
-        } catch (error: any) {
-          console.error("Error: ", error);
-          setErrorMessage(error.response.data.password._errors[0]);
+  const registerUser = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/users`,
+        {
+          name: name,
+          email: email,
+          password: password,
         }
-      };
+      );
+      await logIn();
+    } catch (error: any) {
+      console.error("Error: ", error);
+      setErrorMessage(error.response.data.password._errors[0]);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    return (
-      <>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Register your account
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={registerUser}>
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Register your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={registerUser}>
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="name"
+                  autoComplete="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
             <div>
-                <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
-                  Name
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="name"
-                    name="name"
-                    type="name"
-                    autoComplete="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
               </div>
+            </div>
 
-
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
-                  </label>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="confirm_password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Confirm Password
-                  </label>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="confirm_password"
-                    name="confirm_password"
-                    type="password"
-                    autoComplete="current-confirm_password"
-                    value={confirm_password}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              {errorMessage ? <h3 style={{color: 'red'}}>{errorMessage}</h3>: password && confirm_password && (password === confirm_password ? <h3 style={{color: "green"}}>Passwords Match!</h3> : <h3 style={{color: "red"}} >Passwords Do Not Match!</h3>)} 
-              <div>
-                <button
-                  type="submit"
-                  className={(validPassword ? "bg-indigo-600 hover:bg-indigo-500  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" : "bg-indigo-300 hover:bg-indigo-300") + "flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm"}
-                  disabled={!validPassword}
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Register
-                </button>
+                  Password
+                </label>
               </div>
-            </form>
-            <div className="flex justify-center py-4">
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="confirm_password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Confirm Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="confirm_password"
+                  name="confirm_password"
+                  type="password"
+                  autoComplete="current-confirm_password"
+                  value={confirm_password}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            {errorMessage ? (
+              <h3 style={{ color: "red" }}>{errorMessage}</h3>
+            ) : (
+              password &&
+              confirm_password &&
+              (password === confirm_password ? (
+                <h3 style={{ color: "green" }}>Passwords Match!</h3>
+              ) : (
+                <h3 style={{ color: "red" }}>Passwords Do Not Match!</h3>
+              ))
+            )}
+            <div>
+              <button
+                type="submit"
+                className={
+                  (validPassword
+                    ? "bg-indigo-600 hover:bg-indigo-500  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    : "bg-indigo-300 hover:bg-indigo-300") +
+                  "flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm"
+                }
+                disabled={!validPassword}
+              >
+                Register
+              </button>
+            </div>
+          </form>
+          <div className="flex justify-center py-4">
             <Link href="/">
               <button className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 my-2 px-4 rounded">
                 Back to Main Page
               </button>
             </Link>
           </div>
+        </div>
+      </div>
+      {isLoading && (
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-4 rounded-lg shadow-lg flex items-center space-x-3">
+            <svg
+              className="animate-spin h-5 w-5 text-indigo-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            <span className="text-indigo-600 font-medium">
+              Creating account...
+            </span>
           </div>
         </div>
-      </>
-    )
-  }
-  
+      )}
+    </>
+  );
+}
