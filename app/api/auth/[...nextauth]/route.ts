82a5e305a89541d65975123edd9c7e87de795e42
ewaldma7 <ewaldma7@jhu.edu>
2025-02-07 14:52:05 -0500

    debugging

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -20,7 +20,7 @@ const handler = NextAuth({
         }
 
         try {
-
+          console.log("API URL:", process.env.NEXT_PUBLIC_API_URL + "/login");
           const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/login", {
             method: "POST",
             headers: {
