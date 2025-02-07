24634470fcae01f1a252e599eefdf92450cfe598
ewaldma7 <ewaldma7@jhu.edu>
2025-02-07 01:10:06 -0500

    Revert "update env variables in config"
    This reverts commit 3112be01504d732fc857750538686c5879d9ebf1.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    // Get base URL depending on environment
-    baseUrl:
-      process.env.VERCEL_ENV === "production"
-        ? "https://www.ethansdailygames.com"
-        : process.env.VERCEL_ENV === "preview"
-        ? `https://${process.env.VERCEL_URL}`
-        : "http://localhost:3000",
-  },
-};
+const nextConfig = {}
 
-// Set derived URLs
-nextConfig.env.NEXT_PUBLIC_API_URL = `${nextConfig.env.baseUrl}/api`;
-nextConfig.env.NEXTAUTH_URL = nextConfig.env.baseUrl;
-
-module.exports = nextConfig;
+module.exports = nextConfig
