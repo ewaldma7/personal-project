3112be01504d732fc857750538686c5879d9ebf1
ewaldma7 <ewaldma7@jhu.edu>
2025-02-07 01:03:19 -0500

    update env variables in config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    // Get base URL depending on environment
+    baseUrl:
+      process.env.VERCEL_ENV === "production"
+        ? "https://www.ethansdailygames.com"
+        : process.env.VERCEL_ENV === "preview"
+        ? `https://${process.env.VERCEL_URL}`
+        : "http://localhost:3000",
+  },
+};
 
-module.exports = nextConfig
+// Set derived URLs
+nextConfig.env.NEXT_PUBLIC_API_URL = `${nextConfig.env.baseUrl}/api`;
+nextConfig.env.NEXTAUTH_URL = nextConfig.env.baseUrl;
+
+module.exports = nextConfig;
